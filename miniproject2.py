63fe5f90d6cde6de7571e60841c602cbc16acce6
aishwaryakumara <aishwaryakumar.a4@gmail.com>
2024-10-28 20:27:07 -0400

    New File

---
+++ miniproject2.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+client = OpenAI(
+  api_key= os.getenv("OPENAI_API_KEY")
+)
+
+# Make a simple API call
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[{"role": "system", "content": "You are a creative writer who talks like Dumbledore from Harry Potter. Give me 5 sentences"},
+              {"role": "user", "content": "Write me a Hello World message"}],
+    # max_tokens=100
+)
+print(response.choices[0].message.content)
